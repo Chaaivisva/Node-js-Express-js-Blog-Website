b633802a247892873cf3dd900169932911cc39a8
Chaaivisva <126050738+Chaaivisva@users.noreply.github.com>
2024-05-01 10:00:33 +0530

    search Bar Added

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ const port = 5000 || process.env.PORT;
 //Connect to DB
 connectDB();
 
+app.use(express.urlencoded({ extended:true }));
+app.use(express.json());
+
 app.use(express.static('public'));
 
 //Template Engine
